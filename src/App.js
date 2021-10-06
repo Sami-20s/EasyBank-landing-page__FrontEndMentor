1ec2e62590445295ff541a0e1f57142f6b19fae1
Sami-20s <samikfouri22@gmail.com>
2021-10-06 21:46:40 +0300

    🚀 Init The Porject

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./components/Home/Home";
+import "./style/style.scss";
+function App() {
+    return (
+        <>
+            <Home />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header/Header";
+import Nav from "./Nav/Nav";
+
+const Home = () => {
+    return (
+        <>
+            <Nav />
+            <Header />
+        </>
+    );
+};
+
+export default Home;
